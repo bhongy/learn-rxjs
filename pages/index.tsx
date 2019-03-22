f8eb649d27ebe44b32c571a70778718268db49bc
Thanik Bhongbhibhat <email@thanik.me>
2019-03-21 19:22:30 -0700

    Update index.tsx and remove old files (components)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import './index.css';
 
-const Homepage = () => <div>Homepage</div>;
+const Homepage = () => (
+  <>
+    <Head>
+      <title>Learn RxJS: Homepage</title>
+    </Head>
+    <main>
+      <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/"><a>Home</a></Link></li>
+      </ul>
+    </main>
+  </>
+);
 
 export default Homepage;
